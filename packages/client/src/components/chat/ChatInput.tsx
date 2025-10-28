d8a5e8f898755e9ab9740a0a57829260bbd6a3b7
RemyaManoharan <remyajinil@outlook.com>
2025-10-28 09:21:39 +0100

    completed the project

---
+++ packages/client/src/components/chat/ChatInput.tsx
@@ -0,0 +1,49 @@
+import { Button } from '../ui/button';
+import { FaArrowUp } from 'react-icons/fa';
+import { useForm } from 'react-hook-form';
+import type { KeyboardEvent } from 'react';
+
+export type ChatFormData = {
+   prompt: string;
+};
+type Props = {
+   onSubmit: (data: ChatFormData) => void;
+};
+const ChatInput = ({ onSubmit }: Props) => {
+   const { register, handleSubmit, reset, formState } = useForm<ChatFormData>();
+
+   const onFormSubmit = handleSubmit((data) => {
+      reset({ prompt: '' });
+      onSubmit(data);
+   });
+   const handleKeyDown = (e: KeyboardEvent<HTMLFormElement>) => {
+      if (e.key === 'Enter' && !e.shiftKey) {
+         e.preventDefault();
+         onFormSubmit();
+      }
+   };
+
+   return (
+      <form
+         onSubmit={onFormSubmit}
+         onKeyDown={handleKeyDown}
+         className="flex flex-col gap-2 items-end border-2 p-4 rounded-3xl"
+      >
+         <textarea
+            {...register('prompt', {
+               required: true,
+               validate: (value) => value.trim().length > 0,
+            })}
+            autoFocus
+            className="w-full border-0 outline-none focus:outline-none resize-none"
+            placeholder="Ask anything"
+            maxLength={1000}
+         />
+         <Button disabled={!formState.isValid} className="rounded-full w-9 h-9">
+            <FaArrowUp />
+         </Button>
+      </form>
+   );
+};
+
+export default ChatInput;
